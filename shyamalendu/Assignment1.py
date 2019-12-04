bd732159ad14e66a093ea3a511f893a1a3b03b83
shyamalendumanna <38295511+shyamalendumanna@users.noreply.github.com>
2019-12-04 12:11:17 +0530

    Add files via upload

---
+++ shyamalendu/Assignment1.py
@@ -0,0 +1,49 @@
+#How to iterate through a list. Want the position and value?
+#Given_list = [9,8,7,6,5,4,3]
+# Python3 code to iterate over a list 
+list = [9,8,7,6,5,4,3] 
+   
+# Using enumerate()  
+for i, val in enumerate(list): 
+    print (i, ",",val)
+
+#Rotation of a string?
+"""Input_str = “this is given string”
+This function should return “is given string this ” """
+#Find the given number is prime?
+
+#Writing Fibonacci series
+
+
+#Python program to check if a string is palindrome or not
+
+#Python Program for Program to find the area of a circle
+"""Area = pi * r2
+where r is radius of circle
+PI = 3.142"""
+import math
+r = float(input('Enter the radius of the circle :'))
+area = math.pi * r * r
+print("Area of the circle is : %.2f" %area)
+#Python Program for Sum of squares of first n natural numbers
+
+
+#Python program to print all Prime numbers in an Interval
+
+
+#Python program to swap two elements in a list
+
+
+#Python | Reversing a List
+"""
+Input : list = [4, 5, 6, 7, 8, 9]
+Output : [9, 8, 7, 6, 5, 4] 
+"""
+
+
+#Python | Cloning or Copying a list
+
+
+#Reverse words in a given String in Python
+"""Input : str = "Indian Statistical Institute - Tezpur sample python code"
+Output : str = "code python sample Tezpur - Institute Statistical Indian"  """
